107bf51989a4fc2b39adb834937ee7e087a997f3
Rodrigo Beierstdt <rodrigobeierstdtcosta@gmail.com>
2026-04-28 16:47:01 -0300

    Add files via upload

---
+++ Calculadora/src/Cargo/Main.java
@@ -0,0 +1,27 @@
+package Cargo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> number = new ArrayList<>();
+
+        int i = 0;
+        while (i < 250){
+          number.add("Número " + i);
+            i++;
+
+
+        }
+            System.out.println(number.get(0));
+            }
+
+        }
+
+
+
+
+
+        
